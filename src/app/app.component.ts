9036941635e32442aa248194eb3c73b4f0482430
sharmilaraman <sharmila05122002@gmail.com>
2025-02-09 20:44:02 +0530

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+import { HttpClientModule } from '@angular/common/http';
+
+import { CommonModule } from '@angular/common';
+import { TaskComponent } from './task/task.component';  // Import standalone component
+
+
 
 @Component({
+  standalone: true,
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CommonModule,TaskComponent,HttpClientModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'todo';
+  title = 'todoapp';
+  faCoffee = faCoffee;
+
 }
---
+++ src/app/app.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+
+@NgModule({
+  
+  declarations:[],
+  imports: [BrowserModule,RouterModule],
+  bootstrap: []
+})
+export class AppModule {
+  
+} 
